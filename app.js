daf96da61cda4fce3e500f494093670802c005c7
shaexintran <shaexin@icloud.com>
2025-04-18 13:40:26 -0700

    Completed level up challenges: multi-digit support and checking for NaN or infinity

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /* 
+
 COMPLETED USER STORIES:
 * As a user, I want to be able to select numbers so that I can perform operations with them.
 * As a user, I want to be able to add two numbers together.
@@ -8,25 +9,10 @@ COMPLETED USER STORIES:
 * As a user, I want to be able to see the output of the mathematical operation.
 * As a user, I want to be able to clear all operations and start from 0.
 
-
-LEVEL UP CHALLENGE 1: 
+COMPLETED * LEVEL UP CHALLENGE 1: 
 A. Improve the code to accept multi-digit numbers for the calculations.
 
-pseudo-code:
-
-while operator is null (no operation has been selected yet), then
-string the numbers selected together as a firstNumber
-
-once an operator has been selected, then
-string any numbers selected after as a secondNumber 
-before the = has been selected
-
-we want the numbers pressed to keep appending to each other as multi-digits
-ex. pressing buttons 1, 2, 6, 8 should equal 1268 on display
-this should also be the full number that is operated on
-
-
-LEVEL UP CHALLENGE 2:
+COMPLETED * LEVEL UP CHALLENGE 2:
 B. Have the recent total become the first number to operate again
 
 */
@@ -37,7 +23,7 @@ const displayDiv = document.querySelector(".display")
 
 /*-------------------------------- Variables --------------------------------*/
 // To perform a calculation, we need to know what numbers and which operation
-let firstNumber = null
+let firstNumber = null;
 let secondNumber = null
 let operator = null
 
@@ -58,17 +44,32 @@ buttons.forEach((button) => {
 
         // if button has class of number, save what number it is
         if (buttonClasses.includes('number')) {
-            if (firstNumber == null) {
-                firstNumber = event.target.innerText
-                displayDiv.textContent += firstNumber
-                // log to check
-                console.log(`you logged 1st number: ${firstNumber} typeof ${typeof firstNumber}`)
+            // if the operator has not been selected 
+            if (operator == null) {
+                if (firstNumber == null) {
+                    firstNumber = event.target.innerText
+                    console.log(`you logged a number: ${firstNumber}`) // check
+                 }
+                else {
+                    firstNumber += event.target.innerText
+                    console.log(`${firstNumber}`) // check
+                } 
+                displayDiv.textContent = firstNumber
+            } 
+            // operator has been selected
+            else {
+                if (secondNumber == null) {
+                    secondNumber = event.target.innerText
+                    console.log(`you logged a 2nd number: ${secondNumber}`) // check
+                 }
+                else {
+                    secondNumber += event.target.innerText
+                    console.log(`${secondNumber}`) // check
+                } 
+                displayDiv.textContent = secondNumber
             }
-            secondNumber = event.target.innerText
-            displayDiv.textContent = secondNumber
-            // log to check
-            console.log(`you logged 2nd number: ${secondNumber} typeof ${typeof secondNumber}`)
         } 
+
         // if button has class of operator, save what operator it is
         else if (buttonClasses.includes('operator')) {
             operator = event.target.innerText
@@ -87,10 +88,17 @@ buttons.forEach((button) => {
         }
         // else, pressing = equals will calculate the total
         else {
-            displayDiv.textContent = null
-            calculation()
+            const resultNumber = calculation()
+            
             // this resets the numbers and operators for the next calculation
-            firstNumber = secondNumber = operator = null
+            if (resultNumber) {
+                firstNumber = resultNumber
+                secondNumber = operator = null
+            }
+            else {
+                firstNumber = secondNumber = operator = null
+            }
+
             // log to check
             console.log(firstNumber, secondNumber, operator) 
             console.log('do another one!')
@@ -108,31 +116,37 @@ function calculation() {
         let total;
 
         // check the operator and return the correct total
+
         switch (operator) {
             case '+':
-                displayDiv.textContent = null
+                //displayDiv.textContent = null
                 total = firstNumber + secondNumber
-                displayDiv.textContent += total
+                displayDiv.textContent = total
                 console.log(`your addition total is: ${total}`)
-                break;
+                return total;
             case '-':
-                displayDiv.textContent = null
+                //displayDiv.textContent = null
                 total = firstNumber - secondNumber
-                displayDiv.textContent += total
+                displayDiv.textContent = total
                 console.log(`your subtraction difference is: ${total}`)
-                break;
+                return total;
             case '*':
-                displayDiv.textContent = null
+                //displayDiv.textContent = null
                 total = firstNumber * secondNumber
-                displayDiv.textContent += total
+                displayDiv.textContent = total
                 console.log(`your multiplication product is: ${total}`)
-                break;
+                return total;
             case '/':
-                displayDiv.textContent = null
+                //displayDiv.textContent = null
+                if (!secondNumber) {
+                    displayDiv.textContent = "Try again."
+                    console.log('this will result in NaN or infinity')
+                    return;
+                }
                 total = firstNumber / secondNumber
-                displayDiv.textContent += total
+                displayDiv.textContent = total
                 console.log(`your division result is: ${total}`)
-                break;
+                return total;
         }
     }
 }
