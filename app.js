efc795aa309b25fa1e0a889755c57cee6c897784
shaexintran <shaexin@icloud.com>
2025-04-19 11:19:32 -0700

    Fix toggling plusminus on second number now removes or adds, allowing decimals after a 0 on first number

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+/*------------------------------ Improvements -------------------------------*/
+
+/* 
+An improvement would be refactoring the code to reduce redundancy. Lots of
+'checks' for whether something is null, true, or else are repeated and can be
+abstracted somehow. Think how to consolidate repeating actions.
+
+Other repetitive actions:
+* If something results to a single 0, we display 0 but reset variables to null.
+* Checking for number buttons, we are always checking to see whether a number
+is null (not selected), true (selected), or '0'.
+* Checking if a number is a numOne or numTwo happens multiple times.
+*/
+
 /*-------------------------------- Constants --------------------------------*/
 
 // Saving the div with class of display 
@@ -26,31 +40,43 @@ buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
         // this makes an array of classes from the button that was pressed
         const buttonClass = Array.from(button.classList)
+        const eventText = event.target.innerText
 
-        // if button has class of number, save what number it is
+        // if button has class of number
         if (buttonClass.includes('number')) {
-            // and if the operator has not been selected 
+            // and if the operator has not been selected, it's 1st number
             if (operator == null) {
-                if (numOne == null || numOne == '0') {
-                    numOne = event.target.innerText
+                // if number is null or number is 0
+                if (numOne == null || numOne === '0') {
+                    // unless the number is a period, do this instead
+                    if (eventText === '.') {
+                        numOne = '0' + eventText
+                        display.textContent = '0.'
+                        console.log('this should be 0.')
+                        return
+                    }
+                    // save the number
+                    numOne = eventText
                     console.log(`you logged a number: ${numOne}`) // check
                  }
+                 // add the next numbers to 1st number
                 else {
-                    numOne += event.target.innerText
+                    numOne += eventText
                     console.log(`it is now ${numOne}`) // check
                 } 
                 display.textContent = numOne
             } 
-            // else if operator has been selected, save the second number
+            // else if operator has been selected, it's the 2nd number
             else {
+                // this saves the first numeral digit of 2nd number
                 if (numTwo == null) {
-                    numTwo = event.target.innerText
+                    numTwo = eventText
                     console.log(`you logged a 2nd number: ${numTwo}`) // check
                  }
+                 // add the next numbers to 2nd number
                 else {
-                    // this displays the number before it is added
-                    display.textContent += event.target.innerText
-                    numTwo += event.target.innerText
+                    display.textContent += eventText
+                    numTwo += eventText
                     console.log(`it is now ${numTwo}`) // check
                     return
                 } 
@@ -69,32 +95,36 @@ buttons.forEach((button) => {
             // if there's already two numbers chosen
             else if (numOne && numTwo) {
                 console.log('we should perform the previous operation first')
-                const anotherResult = calculate()
-                // reset if calculation came back undefined (divided by 0)
-                if (anotherResult == undefined) {
+                const previousTotal = calculate()
+                // check if calculation came back undefined (divided by 0)
+                if (!previousTotal) {
+                    // reset instead
                     numOne = numTwo = operator = null
+                    console.log(`previous total was ${previousTotal}, resetting to null`) // check
+                    console.log('reset:', numOne, numTwo, operator)
                     return
                 }
-                numOne = anotherResult
+                // continue if previousTotal was true, save as 1st number
+                numOne = previousTotal
                 numTwo = null
-                operator = event.target.innerText
+                operator = eventText
                 display.textContent = `${numOne} ${operator} `
                 // log to check
                 console.log('saved as: ', numOne, numTwo, operator) 
                 return
             }  // if operator has already been chosen
             else if (operator) {
-                console.log(`there's already an operator here. switching it`)
-                operator = event.target.innerText
+                operator = eventText
                 display.textContent = `${numOne} ${operator} `
                 console.log('updated operator to:', operator) // check
                 return
             }
-            operator = event.target.innerText
+            operator = eventText
             display.textContent += ` ${operator} `
             console.log(`you logged an operator: ${operator}`) // check
         }
-        // if operator is C - reset the entries
+
+        // if operator is clear - reset the entries
         else if (buttonClass.includes('clear')) {
             display.textContent = 0
             numOne = numTwo = operator = null
@@ -113,7 +143,7 @@ buttons.forEach((button) => {
                 else if (numOne) {
                     numOne = Number(numOne) * -1
                     display.textContent = numOne
-                    console.log(`making it -${numOne}`)
+                    console.log(`inversing to ${numOne}`)
                 }
                 else {
                     numOne = null
@@ -124,7 +154,19 @@ buttons.forEach((button) => {
             // else, its the second one 
             else {
                 // places the plusminus on an existing number
-                if (numTwo) {
+                if (numTwo == '0') {
+                    console.log(`this is a 0, cannot make negative.`)
+                    display.textContent = numTwo
+                    numOne = numTwo = operator = null
+                    // log to check
+                    console.log('reset as: ', numOne, numTwo, operator)
+                }
+                else if (numTwo == '-') {
+                    console.log(`pressing plusmins after creating a minus on 2nd number`)
+                    numTwo = null
+                    display.textContent = `${numOne} ${operator} `
+                }
+                else if (numTwo) {
                     numTwo = Number(numTwo) * -1
                     display.textContent = `${numOne} ${operator} ${numTwo}`
                     console.log(`making it -${numTwo}`)
@@ -177,16 +219,19 @@ function calculate() {
         } else if (operator == '*') {
             total = numOne * numTwo
         } else if (operator == '/') {
+            // if numTwo is falsy (0)
             if (!numTwo) {
                 display.textContent = `Can't divide by 0.`
-                console.log('this will result in NaN or infinity')
+                console.log('this will result in NaN or infinity. display: cannot divide message')
                 return
             }
             total = numOne / numTwo
         }
         console.log(`your total is: ${total}`)
+        // check if result is 0, return as string to avoid falsy
         if (total === 0) {
             total = '0'
+            console.log('result was 0, passing instead:', typeof total)
         }
         return total
     }
